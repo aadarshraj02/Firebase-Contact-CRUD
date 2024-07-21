4cb82871ea80f50b8c3277355c867b1fbfd147ff
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-21 10:59:15 +0530

    moved cards code in contact card component

--- src/App.jsx
+++ src/App.jsx
@@ -45,22 +45,7 @@ function App() {
       </div>
       <div>
         {contacts.map((contact) => (
-          <div
-            key={contact.id}
-            className="my-3 flex justify-between rounded-lg bg-yellow p-3"
-          >
-            <div className="flex gap-2">
-              <HiOutlineUserCircle className="text-4xl text-orange" />
-              <div className="">
-                <h2 className="font-bold">{contact.name}</h2>
-                <p className="text-sm">{contact.email}</p>
-              </div>
-            </div>
-            <div className="flex items-center text-3xl">
-              <RiEditCircleLine />
-              <IoMdTrash className="text-orange" />
-            </div>
-          </div>
+       
         ))}
       </div>
     </div>
--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,5 +1,23 @@
-const ContactCard = () => {
-  return <div></div>;
+/* eslint-disable react/prop-types */
+const ContactCard = ({contact}) => {
+  return (
+    <div
+      key={contact.id}
+      className="my-3 flex justify-between rounded-lg bg-yellow p-3"
+    >
+      <div className="flex gap-2">
+        <HiOutlineUserCircle className="text-4xl text-orange" />
+        <div className="">
+          <h2 className="font-bold">{contact.name}</h2>
+          <p className="text-sm">{contact.email}</p>
+        </div>
+      </div>
+      <div className="flex items-center text-3xl">
+        <RiEditCircleLine />
+        <IoMdTrash className="text-orange" />
+      </div>
+    </div>
+  );
 };
 
 export default ContactCard;
