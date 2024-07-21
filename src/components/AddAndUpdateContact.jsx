3b2f1f0dff3ac45ffd7a536453b8ffb030ba300e
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-21 12:21:52 +0530

    added input for email

--- src/components/AddAndUpdateContact.jsx
+++ src/components/AddAndUpdateContact.jsx
@@ -9,7 +9,11 @@ function AddAndUpdateContact({ isOpen, onClose }) {
           <Form>
             <div className="flex flex-col gap-1">
               <label htmlFor="name">Name</label>
-              <Field name="name" />
+              <Field name="name" className="rounded-md border" />
+            </div>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="email">Email</label>
+              <Field type="email" name="email" className="rounded-md border" />
             </div>
           </Form>
         </Formik>
