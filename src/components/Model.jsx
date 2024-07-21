ecd3420d3429cead082c0923fab570f43e793137
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-21 11:49:47 +0530

    created a modal div and added icon close and added on click

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,5 +1,21 @@
+/* eslint-disable no-unused-vars */
+import { AiOutlineClose } from "react-icons/ai";
+
 function Model({ onClose, isOpen, children }) {
-  return <>{isOpen && <div>Modal</div>}</>;
+  return (
+    <>
+      {isOpen && (
+        <div className="min-h-[200px] max-w-[80%] bg-white p-4">
+          <div className="flex justify-end">
+            <AiOutlineClose
+              onClick={onClose}
+              className="cursor-pointer text-3xl"
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Model;
