3b61d0e4954419032139a6b7e91411a13a1028ea
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-21 11:57:42 +0530

    added portal

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable no-unused-vars */
+import { createPortal } from "react-dom";
 import { AiOutlineClose } from "react-icons/ai";
 
 function Model({ onClose, isOpen, children }) {
-  return (
+  return createPortal(
     <>
       {isOpen && (
         <>
-          <div className="relative z-20 min-h-[300px] max-w-[80%] bg-white p-4">
+          <div className="relative z-20 mx-auto min-h-[300px] max-w-[80%] bg-white p-4">
             <div className="flex justify-end">
               <AiOutlineClose
                 onClick={onClose}
@@ -18,7 +19,8 @@ function Model({ onClose, isOpen, children }) {
           <div className="absolute top-0 z-10 h-screen w-screen backdrop-blur-md" />
         </>
       )}
-    </>
+    </>,
+    document.getElementById("model-root"),
   );
 }
 
