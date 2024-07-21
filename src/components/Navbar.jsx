726250e85e299ced12f96860dd5989c5d8fe323e
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-21 08:35:49 +0530

    styled nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,8 @@
 const Navbar = () => {
   return (
-    <nav className="h-[60px] bg-white m-4 rounded-lg ">
-      <div>
-        <img src="/firebase.svg" alt="" />
-        <h1>Firebase Contact</h1>
-      </div>
+    <nav className="h-[60px] bg-white m-4 rounded-lg flex justify-center items-center gap-2">
+      <img src="/firebase.svg" alt="" />
+      <h1 className="text-xl font-medium">Firebase Contact</h1>
     </nav>
   );
 };
