2451ecee5d3731f5cee990f7d5df4d93e574e58d
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-21 10:13:09 +0530

    added use effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { AiFillPlusCircle } from "react-icons/ai";
 import Navbar from "./components/Navbar";
 import { FiSearch } from "react-icons/fi";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(()=>{},[])
+
   return (
     <div className="mx-auto max-w-[370px] px-4">
       <Navbar></Navbar>
