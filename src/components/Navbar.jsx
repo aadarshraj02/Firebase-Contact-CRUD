d936fef509deecad81b9f42d833a593ca6934ec8
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-21 10:06:10 +0530

    created prettierrc

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="h-[60px] bg-white my-4 rounded-lg flex justify-center items-center gap-2">
+    <nav className="my-4 flex h-[60px] items-center justify-center gap-2 rounded-lg bg-white">
       <img src="/firebase.svg" alt="" />
       <h1 className="text-xl font-medium">Firebase Contact</h1>
     </nav>
