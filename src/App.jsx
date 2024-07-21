8d8afe6887a92f2a398d387cc716dceb12675d71
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-21 09:51:14 +0530

    added a search icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
+import { FiSearch } from "react-icons/fi";
 
 function App() {
   return (
     <div className="max-w-[370px] mx-auto px-4">
       <Navbar></Navbar>
       <div className="flex ">
+        <FiSearch />
         <input
           type="text"
           className="bg-transparent border border-white rounded-md h-10 flex-grow"
