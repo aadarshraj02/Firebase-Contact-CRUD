f69315e690e6051586e3e1f6f1f2739202f02763
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-21 14:47:57 +0530

    added image and text to display on not found

--- src/components/NotFoundContact.jsx
+++ src/components/NotFoundContact.jsx
@@ -1,11 +1,10 @@
-
-
 function NotFoundContact() {
   return (
     <div>
-      
+      <img src="/contact.png" alt="" />
+      <h3>Contact Not Found!</h3>
     </div>
-  )
+  );
 }
 
-export default NotFoundContact
+export default NotFoundContact;
