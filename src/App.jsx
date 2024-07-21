7eae477601db612fc2db1d9da972cfe6d99be7df
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-21 10:52:05 +0530

    styled card to display contat details

--- src/App.jsx
+++ src/App.jsx
@@ -45,17 +45,20 @@ function App() {
       </div>
       <div>
         {contacts.map((contact) => (
-          <div key={contact.id} className="bg-yellow">
-            <div>
+          <div
+            key={contact.id}
+            className="my-3 flex justify-between rounded-lg bg-yellow p-3"
+          >
+            <div className="flex gap-2">
               <HiOutlineUserCircle className="text-4xl text-orange" />
               <div className="">
-                <h2>{contact.name}</h2>
-                <p>{contact.email}</p>
+                <h2 className="font-bold">{contact.name}</h2>
+                <p className="text-sm">{contact.email}</p>
               </div>
             </div>
-            <div>
+            <div className="flex items-center text-3xl">
               <RiEditCircleLine />
-              <IoMdTrash />
+              <IoMdTrash className="text-orange" />
             </div>
           </div>
         ))}
