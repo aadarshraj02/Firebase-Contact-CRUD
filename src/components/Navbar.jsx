8acc3bc4d84f9caccf3304a6e7ba025c7e252c0d
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-21 08:33:15 +0530

    added h1 and image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,12 @@
 const Navbar = () => {
-  return <nav className="h-[60px] bg-white m-4 rounded-lg "></nav>;
+  return (
+    <nav className="h-[60px] bg-white m-4 rounded-lg ">
+      <div>
+        <img src="/firebase.svg" alt="" />
+        <h1>Firebase Contact</h1>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
