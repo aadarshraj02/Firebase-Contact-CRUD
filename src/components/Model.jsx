60ac3a97347d80914aca18520a6bee80b0950f08
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-21 11:54:43 +0530

    applied blur

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -5,14 +5,18 @@ function Model({ onClose, isOpen, children }) {
   return (
     <>
       {isOpen && (
-        <div className="min-h-[200px] max-w-[80%] bg-white p-4">
-          <div className="flex justify-end">
-            <AiOutlineClose
-              onClick={onClose}
-              className="cursor-pointer text-3xl"
-            />
+        <>
+          <div className="relative z-20 min-h-[300px] max-w-[80%] bg-white p-4">
+            <div className="flex justify-end">
+              <AiOutlineClose
+                onClick={onClose}
+                className="cursor-pointer text-3xl"
+              />
+            </div>
           </div>
-        </div>
+
+          <div className="absolute top-0 z-10 h-screen w-screen backdrop-blur-md" />
+        </>
       )}
     </>
   );
