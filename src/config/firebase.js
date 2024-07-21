f44f9a9025c24d48ae9388c026b956dec9f8d1a0
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-21 07:53:01 +0530

    created firbase.js

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBG_e-JWDhamC_eeUtBGoLgkQUxSiMop8A",
+  authDomain: "vite-contact-df82f.firebaseapp.com",
+  projectId: "vite-contact-df82f",
+  storageBucket: "vite-contact-df82f.appspot.com",
+  messagingSenderId: "710303127941",
+  appId: "1:710303127941:web:332372ed51a6b58c744cec",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
