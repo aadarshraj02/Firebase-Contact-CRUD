3a50efb2e0bcb932c34ca7a020f6cd6a568880ae
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-21 10:11:37 +0530

    styled plus icon

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,15 @@ function App() {
   return (
     <div className="mx-auto max-w-[370px] px-4">
       <Navbar></Navbar>
-      <div>
-        <div className="relative flex items-center justify-end">
+      <div className="flex gap-2">
+        <div className="relative flex flex-grow items-center justify-end">
           <FiSearch className="absolute mr-2 text-3xl text-white" />
           <input
             type="text"
             className="h-10 flex-grow rounded-md border border-white bg-transparent p-2 pr-10 text-white outline-none"
           />
         </div>
-        <div>
-          <AiFillPlusCircle />
-        </div>
+        <AiFillPlusCircle className="cursor-pointer text-5xl text-white" />
       </div>
     </div>
   );
