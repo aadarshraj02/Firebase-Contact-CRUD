7e432bf3b6d11af16aaaaa1be6f86e23a85967bf
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-21 09:47:20 +0530

    added input and styled

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,14 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="max-w-[370px] mx-auto">
+    <div className="max-w-[370px] mx-auto px-4">
       <Navbar></Navbar>
+      <div className="flex ">
+        <input
+          type="text"
+          className="bg-transparent border border-white rounded-md h-10 flex-grow"
+        />
+      </div>
     </div>
   );
 }
