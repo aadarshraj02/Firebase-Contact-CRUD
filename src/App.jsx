ab1db677f67db9278efae1466ade2bf4142e9d49
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-21 10:32:54 +0530

    added a icon to display user

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { FiSearch } from "react-icons/fi";
 import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "./config/firebase";
+import { HiOutlineUserCircle } from "react-icons/hi";
 
 function App() {
   const [contacts, setContacts] = useState([]);
@@ -39,6 +40,14 @@ function App() {
           />
         </div>
         <AiFillPlusCircle className="cursor-pointer text-5xl text-white" />
+      </div>
+      <div>
+        {contacts.map((contact) => (
+          <div key={contact.id}>
+            <HiOutlineUserCircle />
+            
+          </div>
+        ))}
       </div>
     </div>
   );
