8f8f33e3a43c5e94f0271c1f51971a960cbb9c13
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-21 14:56:53 +0530

    styledimg h3 and div

--- src/components/NotFoundContact.jsx
+++ src/components/NotFoundContact.jsx
@@ -1,8 +1,8 @@
 function NotFoundContact() {
   return (
-    <div>
+    <div className="m-auto mt-36 flex items-center justify-center gap-4">
       <img src="/contact.png" alt="" />
-      <h3>Contact Not Found!</h3>
+      <h3 className="text-xl font-medium text-white">Contact Not Found!</h3>
     </div>
   );
 }
