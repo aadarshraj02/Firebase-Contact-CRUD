ea71a9813839aafe29166a7399365bc291d4a1b7
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-21 11:38:33 +0530

    wrappped in react fragment

--- src/App.jsx
+++ src/App.jsx
@@ -37,25 +37,28 @@ function App() {
   }, []);
 
   return (
-    <div className="mx-auto max-w-[370px] px-4">
-      <Navbar></Navbar>
-      <div className="flex gap-2">
-        <div className="relative flex flex-grow items-center justify-end">
-          <FiSearch className="absolute mr-2 text-3xl text-white" />
-          <input
-            placeholder="search contact"
-            type="text"
-            className="h-10 flex-grow rounded-md border border-white bg-transparent p-2 pr-10 text-white outline-none"
-          />
+    <>
+      {" "}
+      <div className="mx-auto max-w-[370px] px-4">
+        <Navbar></Navbar>
+        <div className="flex gap-2">
+          <div className="relative flex flex-grow items-center justify-end">
+            <FiSearch className="absolute mr-2 text-3xl text-white" />
+            <input
+              placeholder="search contact"
+              type="text"
+              className="h-10 flex-grow rounded-md border border-white bg-transparent p-2 pr-10 text-white outline-none"
+            />
+          </div>
+          <AiFillPlusCircle className="cursor-pointer text-5xl text-white" />
         </div>
-        <AiFillPlusCircle className="cursor-pointer text-5xl text-white" />
+        <div>
+          {contacts.map((contact) => (
+            <ContactCard key={contact.id} contact={contact} />
+          ))}
+        </div>
       </div>
-      <div>
-        {contacts.map((contact) => (
-          <ContactCard key={contact.id} contact={contact} />
-        ))}
-      </div>
-    </div>
+    </>
   );
 }
 
