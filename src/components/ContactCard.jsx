4c572ce57fb6e28fb972c5b2a1ee5441b9fbe68a
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-21 13:34:16 +0530

    imported Addand Updatecontact

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -3,6 +3,7 @@ import { HiOutlineUserCircle } from "react-icons/hi";
 import { IoMdTrash } from "react-icons/io";
 import { LiaUserEditSolid } from "react-icons/lia";
 import { db } from "../config/firebase";
+import AddAndUpdateContact from "./AddAndUpdateContact";
 
 const COntactCard = ({ contact }) => {
   const deleteContact = async (id) => {
@@ -14,25 +15,28 @@ const COntactCard = ({ contact }) => {
   };
 
   return (
-    <div
-      key={contact.id}
-      className="my-3 flex justify-between rounded-lg bg-yellow p-3"
-    >
-      <div className="flex gap-2">
-        <HiOutlineUserCircle className="text-4xl text-orange" />
-        <div className="">
-          <h2 className="font-bold">{contact.name}</h2>
-          <p className="text-sm">{contact.email}</p>
+    <>
+      <div
+        key={contact.id}
+        className="my-3 flex justify-between rounded-lg bg-yellow p-3"
+      >
+        <div className="flex gap-2">
+          <HiOutlineUserCircle className="text-4xl text-orange" />
+          <div className="">
+            <h2 className="font-bold">{contact.name}</h2>
+            <p className="text-sm">{contact.email}</p>
+          </div>
+        </div>
+        <div className="flex items-center gap-2 text-3xl">
+          <LiaUserEditSolid className="cursor-pointer text-zinc-500" />
+          <IoMdTrash
+            onClick={() => deleteContact(contact.id)}
+            className="cursor-pointer text-orange"
+          />
         </div>
       </div>
-      <div className="flex items-center gap-2 text-3xl">
-        <LiaUserEditSolid className="cursor-pointer text-zinc-500" />
-        <IoMdTrash
-          onClick={() => deleteContact(contact.id)}
-          className="cursor-pointer text-orange"
-        />
-      </div>
-    </div>
+      <AddAndUpdateContact />
+    </>
   );
 };
 
